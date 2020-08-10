cd5d31930add29f015c70631d4b3d19338e97d45
Connor Twele <connortwele@gmail.com>
2020-08-09 21:19:29 -0400

    Updates

---
+++ precache-manifest.65e095896a999e6b522a21edd7494cf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80eb2601ad94c1689f240be0c89880f0",
+    "url": "/hw0808/index.html"
+  },
+  {
+    "revision": "4e7d401c8935c0cca48b",
+    "url": "/hw0808/static/css/main.fcd1e4e8.chunk.css"
+  },
+  {
+    "revision": "6421df66dec9a9a045f7",
+    "url": "/hw0808/static/js/2.499bb148.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw0808/static/js/2.499bb148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7d401c8935c0cca48b",
+    "url": "/hw0808/static/js/main.0561b7fc.chunk.js"
+  },
+  {
+    "revision": "7636b3971d56f827c8ee",
+    "url": "/hw0808/static/js/runtime-main.12fe0834.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/hw0808/static/media/pokeball.f10bf078.png"
+  }
+]);
